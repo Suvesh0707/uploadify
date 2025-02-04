d8cda0cad2e30241d4606bad6b076258cfaa77ef
Suvesh Pagam <156836161+Suvesh0707@users.noreply.github.com>
2025-02-04 11:11:34 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
+dotenv.config({path:"./.env"})
 import connectDB from "./db/index.js"
+import dotenv from "dotenv"
+import app from "./app.js"
+
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT,()=>{
+        console.log(`app is listening at port: ${process.env.PORT}`)
+    })
+})
+.catch((error)=>{
+    console.log("MongoDB connection failed !!", error)
+})
 
