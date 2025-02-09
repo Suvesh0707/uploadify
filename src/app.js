99dab722d94c622f5ccbb40dc71b3d9287c6516e
Suvesh Pagam <156836161+Suvesh0707@users.noreply.github.com>
2025-02-09 12:15:52 +0530

    routes declare

--- src/app.js
+++ src/app.js
@@ -18,5 +18,10 @@ app.use(express.urlencoded({
 app.use(express.static("public"))
 app.use(cookieParser())
 
+
+// routes
+import userRouter from "./routes/user.routes.js"
+app.use("/api/v1/users", userRouter )
+
 export default app
 
